18d90be83a9665bc150f023adb2bec843e5faa9e
rdunavant0 <rdunavant0@gmail.com>
2019-01-24 11:55:05 -0600

    message

---
+++ CW.py
@@ -0,0 +1,31 @@
+def problem1():
+    userInput = ""
+    while(userInput != 'q' or 'Q'):
+        userInput = input("Enter something")
+        myList = userInput
+        if userInput == 'q' or 'Q':
+            print(myList)
+def problem2():
+    myDictionaryList = [
+        {
+            "name": "Kelvin",
+            "age": 30
+        },
+        {
+            "name": "Bob",
+            "age": 50
+        },
+        {
+            "name": "Alex",
+            "age": 21
+        }
+    ]
+    userInput = input("AGE or NAME")
+    print(myDictionaryList)
+    continue
+    while(userInput = getSortKey):
+        print(myDictionaryList.sort)
+    while(userInput != 'q' or 'Q')
+    exit
+    if not 'q' or 'Q', 'AGE' or 'NAME':
+        print("INVALID ENTRY. PLEASE TRY AGAIN")
